e7fed50a93909601f4d1352acba96b9eb133febe
DilshanGDT <tharakadilshan506@gmail.com>
2025-09-04 22:37:18 +0530

    change port, url and read interval

--- client/config.py
+++ client/config.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 class Config:
-    SERIAL_PORT = os.getenv("SERIAL_PORT", "COM3")
+    SERIAL_PORT = os.getenv("SERIAL_PORT", "COM5")
     BAUD_RATE = int(os.getenv("BAUD_RATE", "115200"))
-    SERVER_URL = os.getenv("SERVER_URL", "http://localhost:5000/api/data")
-    READ_INTERVAL = int(os.getenv("READ_INTERVAL", "1"))
+    SERVER_URL = os.getenv("SERVER_URL", "http://localhost:8000/api/data")
+    READ_INTERVAL =float(os.getenv("READ_INTERVAL", "0.01"))

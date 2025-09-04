8316beed2a283712ad6de86376efd872e32876a4
DilshanGDT <tharakadilshan506@gmail.com>
2025-09-04 22:38:14 +0530

    minor changes

--- server/app/main.py
+++ server/app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from .api import router as api_router
@@ -8,4 +9,5 @@ app = FastAPI(title="IoT Dashboard")
 app.include_router(api_router, prefix="/api")
 
 # Serve static dashboard
-app.mount("/", StaticFiles(directory="app/static", html=True), name="static")
+static_dir = os.path.join(os.path.dirname(__file__), "static")
+app.mount("/", StaticFiles(directory=static_dir, html=True), name="static")

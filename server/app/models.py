b9c13f37ab0e1654b5ab360f3bec843507566cbf
DilshanGDT <tharakadilshan506@gmail.com>
2025-09-04 22:38:04 +0530

    add GPS

--- server/app/models.py
+++ server/app/models.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
 from typing import Literal
 
-
 class SensorData(BaseModel):
     humidity: float
     temp_c: float
     temp_f: float
     passengers: int
     distance: float
+    latitude: float
+    longitude: float
     buzzer: Literal["ON", "OFF"]
